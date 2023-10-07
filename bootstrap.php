dc0bef46fbe20a3ad0fa4bb356c23ad42d316c05
pasyalab <webstoryinc@gmail.com>
2023-10-07 21:26:06 +0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -11,4 +11,6 @@ $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
 
 $library_name = 'Perpustakaan Ideal Serbaguna';
 $device_id = 'put_your_device_id_here';
+$api_url = 'https://your-onesender-instance.com/api/v1/messages';
+$api_key = '_your_secure_token_';
 $footer_text = 'Harap simpan resi ini sebagai bukti transaksi.';
